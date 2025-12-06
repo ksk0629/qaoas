dc0523be4e3786f53f4d422992bf9b83737c8fbb
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-12-07 00:50:10 +0900

    #2 Add small test

--- qaoas/qualtran/x_mixer.py
+++ qaoas/qualtran/x_mixer.py
@@ -30,3 +30,30 @@ class XMixer(qlt.Bloq):
             q[index] = bb.add(Rx(parameter), q=q[index])
 
         return {"q": q}
+
+
+if __name__ == "__main__":
+    import cirq
+    import numpy as np
+    from qualtran._infra.gate_with_registers import get_named_qubits
+
+    num_qubits = 3
+    beta = np.pi / 2
+    x_mixer = XMixer(num_qubits=num_qubits, beta=beta)
+
+    cbloq = x_mixer.as_composite_bloq()
+    in_quregs = get_named_qubits(cbloq.signature.lefts())
+    x_mixer_circuit, quregs = cbloq.to_cirq_circuit_and_quregs(**in_quregs)
+
+    qubits = np.asarray(quregs["q"], dtype=object).ravel().tolist()
+
+    circuit = cirq.Circuit(
+        x_mixer_circuit,
+        cirq.measure(*qubits, key="q"),
+    )
+    print(circuit)
+    sim = cirq.Simulator()
+    result = sim.run(circuit, repetitions=500)
+
+    bits = result.measurements["q"]
+    print(bits)
